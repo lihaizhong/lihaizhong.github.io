78c954eaf14bd2b1e5e7150b238e1b148d984d08
SangBaiPi <sangbaipi@2dfire.com>
2019-02-14 15:40:39 +0800

    添加google-analytics

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -2,5 +2,12 @@ module.exports = {
   title: '小白的博客园',
   description: '个人博客',
   // $withBase VuePress内置base引用helper
-  base: '/'
+  base: '/',
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-blog',
+    '@vuepress/plugin-medium-zoom',
+    // '@vuepress/plugin-pwa',
+    ['@vuepress/plugin-google-analytics', { ga: 'UA-134520339-1' }]
+  ]
 }
