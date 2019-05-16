66b49ea920edc1a9de4e9750d5b28a4612761490
SangBaiPi <sangbaipi@2dfire.com>
2019-05-17 02:38:44 +0800

    修改配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -27,12 +27,14 @@ module.exports = {
     app: {
       key: '~/.ssh/host_ecs',
       user: 'root',
-      host: ['120.77.45.219'],
+      host: '120.77.45.219',
+      port: 22,
       ssh_options: 'StrictHostKeyChecking=no',
       ref: 'origin/wechat',
       repo: 'git@github.com:lihaizhong/lihaizhong.github.io.git',
       path: '/home/www/app',
-      'pre-deploy': '',
+      'pre-deploy':
+        'git clone git@github.com:lihaizhong/lihaizhong.github.io.git',
       'post-deploy': ''
     }
   }
