915593f2d4499fe2f3ebfe518597bca2361073dc
SangBaiPi <sangbaipi@2dfire.com>
2019-05-17 12:32:10 +0800

    修改配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,6 +11,17 @@ module.exports = {
       env_production: {
         NODE_ENV: 'production'
       }
+    },
+    {
+      name: 'blog',
+      script: 'serve',
+      args: '--spa index.html ',
+      output: '/home/logs/blog_access.log',
+      error: '/home/logs/blog_error.log',
+      env: {
+        PM2_SERVE_PATH: 'dist/client/',
+        PM2_SERVE_PORT: 8080
+      }
     }
   ],
   deploy: {
@@ -24,7 +35,7 @@ module.exports = {
       path: '/home/www/app',
       'pre-deploy': '',
       'post-deploy':
-        'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production'
+        'npm install --save-prod && npm run build && pm2 startOrRestart ecosystem.config.js --env production'
     }
   }
 }
