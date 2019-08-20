0cb33875da87add98779aa3da1120759207029ec
SangBaiPi <sangbaipi@2dfire.com>
2019-08-20 15:45:11 +0800

    修改配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
       name: 'blog',
       script: path.resolve(__dirname, './node_modules/serve/bin/serve.js'),
-      args: ['dist/client -p 8080 -s'],
+      args: 'dist/client -p 8080 -s',
       output: '/home/logs/blog_access.log',
       error: '/home/logs/blog_error.log'
     }
