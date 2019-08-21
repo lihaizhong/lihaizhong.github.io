431f78793c28b1914e1bff06371001e6a175601c
SangBaiPi <sangbaipi@2dfire.com>
2019-08-21 18:26:31 +0800

    添加ddp代码

---
+++ code/ddp.js
@@ -0,0 +1,16 @@
+const DDPClient = require('ddp')
+
+const client = new DDPClient({
+  host: 'localhost',
+  port: 3000
+})
+
+client.on('message', (data, flags) => {
+  console.log('【DDP 消息】', data, flags)
+})
+
+client.connect(() => {
+  client.subscribe('post', [], () => {
+    console.log('【post 订阅消息】')
+  })
+})
