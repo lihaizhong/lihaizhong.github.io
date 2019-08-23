563f7f3ea392d538ff5749f2b49b84e8e1b0bf91
SangBaiPi <sangbaipi@2dfire.com>
2019-08-23 18:03:24 +0800

    修改editor

--- packages/editor/src/core/ajax.js
+++
@@ -1,9 +0,0 @@
-export default function request(url, options) {
-  return fetch(url, options).then(response => {
-    if (response.ok) {
-      return response
-    } else {
-      throw response
-    }
-  })
-}
