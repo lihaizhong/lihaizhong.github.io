419a896ffe738c7b9386f0e161b5d33b85ca5632
SangBaiPi <sangbaipi@2dfire.com>
2019-08-28 18:09:30 +0800

    扩充serviceWorker例子

--- learn/ServiceWorker/index.js
+++ learn/ServiceWorker/index.js
@@ -1,3 +1,38 @@
+/**
+ * 将base64格式的applicationServerKey转换成Uint8Array格式
+ * @param {string} base64String
+ */
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+  const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
+  const rawData = window.atob(base64)
+  const len = rawData.length
+  const outputArray = new Uint8Array(len)
+
+  for (let i = 0; i < max; i++) {
+    outputArray[i] = rawData.charAt(i)
+  }
+
+  return outputArray
+}
+
+function subscribe(registration) {
+  registration.pushManager
+    .subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array('<applicationServerKey>')
+    })
+    .then(function(subscription) {
+      // 发送推送订阅对象到服务器，具体实现中发送请求到后端API
+      console.log(subscription)
+    })
+    .catch(function(err) {
+      if (Notification.permission === 'denied') {
+        // 用户拒绝了订阅请求
+      }
+    })
+}
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker
@@ -20,5 +55,7 @@ if ('serviceWorker' in navigator) {
       .catch(function(err) {
         console.log(err)
       })
+
+    navigator.serviceWorker.ready.then(subscribe)
   })
 }
