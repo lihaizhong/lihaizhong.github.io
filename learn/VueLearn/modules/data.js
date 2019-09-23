83d6515bbcbb2d5e228ad7750ed3abf17a903f64
sky <854323752@qq.com>
2019-09-24 02:13:55 +0800

    完成vue reactive ppt

--- learn/VueLearn/modules/data.js
+++ learn/VueLearn/modules/data.js
@@ -1,29 +1,67 @@
+/**
+ * 生成常量
+ * @param {object} constants
+ * @return {object} constants
+ */
+function generateConstants(constants) {
+  const nativeType = Object.prototype.toString.call(constants).slice(8);
+  if (['array', 'object'].includes(nativeType)) {
+    const keys = Object.keys(constants);
+
+    keys.forEach((key) =>
+      // 定义数据描述符
+      Object.defineProperty(constants, key, {
+        enumerable: true,
+        configurable: false,
+        value: constants[key],
+        writeable: false,
+      })
+    );
+  }
+
+  return constants;
+}
+
+const CONSTANTS = generateConstants({
+  NO_OPEN: 0,
+  OPEN: 1,
+});
+
 new Vue({
   el: '#app',
   data() {
-    return {
-      name: '桑白皮',
+    const value = {
+      name: 'ggg',
       age: 26,
-      hobby: [
-        '玩游戏',
-        {name: '编程', order: 4},
-        {name: '交朋友', order: 1},
-      ],
+      hobby: ['aaa', {name: 'bbb', order: 4}, {name: 'ccc', order: 1}],
       family: {
         father: 'xxx',
         mother: 'yyy',
       },
     };
+
+    Object.defineProperty(value, 'constants', {
+      enumerable: true,
+      configurable: false,
+      value: CONSTANTS,
+      writeable: false,
+    });
+
+    return value;
   },
-  mounted() {
-    this.name = '小白';
+  created() {
+    // setter
+    this.name = 'hhh';
     this.age = 27;
-    this.hobby.push('睡觉');
+    this.hobby.push('eee');
     this.family.father = 'zzz';
 
-    console.log(this.name);
-    console.log(this.age);
-    console.log(this.hobby);
-    console.log(this.family);
+    setTimeout(() => {
+      // getter
+      console.log(this.name);
+      console.log(this.age);
+      console.log(this.hobby);
+      console.log(this.family);
+    }, 1000);
   },
 });
