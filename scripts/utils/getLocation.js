4abe5fb4d964e91d9f2bcf9b93678c75f9e10b3f
sky <854323752@qq.com>
2019-09-24 10:40:34 +0800

    修改代码位置

--- scripts/utils/getLocation.js
+++ scripts/utils/getLocation.js
@@ -16,9 +16,9 @@ module.exports = function getLocation() {
     const spinner = ora('正在获取当前位置').start();
     const request = http.request(url, options, (response) => {
       console.log(chalk.yellow(`状态码：${response.statusCode}`));
-      console.log(
-        chalk.yellow(`响应头：${JSON.stringify(response.headers, null, 2)}`)
-      );
+      // console.log(
+      //   chalk.yellow(`响应头：${JSON.stringify(response.headers, null, 2)}`)
+      // );
       response.setEncoding('utf8');
 
       let result = '';
@@ -31,11 +31,13 @@ module.exports = function getLocation() {
         result = result ? JSON.parse(result) : {};
         const {location} = result;
 
-        location ?
-          spinner.succeed(`当前位置：${location}`) :
+        if (location) {
+          spinner.succeed(`当前位置：${location}`);
+          resolve(location);
+        } else {
           spinner.fail('获取地理位置失败');
-        // 创建文件
-        resolve(location);
+          reject(new Error('未获取到地理位置信息'));
+        }
       });
     });
 
